9c105d9694d16d32218feb0959cfa2574ca11deb
Nathan <nathan.domenichini@gmail.com>
2025-09-10 16:00:08 +0200

    mmh

--- src/main.py
+++ src/main.py
@@ -30,6 +30,8 @@ DEBUG = os.getenv("DEBUG") == "True"
 # === FLASK APP SETUP ===
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
 
 login_manager = LoginManager()
